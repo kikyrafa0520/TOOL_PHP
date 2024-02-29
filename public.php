7715e989396c4a74cb14a71778a0be2276a201a8
iewilmaestro <purna.iera@gmail.com>
2024-02-29 14:32:33 +0700

    tool sudah siap

---
+++ public.php
@@ -0,0 +1,65 @@
+<?php
+error_reporting(0);
+/*
+function rrmdir($dir) {
+    foreach(glob($dir . '/*') as $file) {
+        if(is_dir($file))
+            rrmdir($file);
+        else
+            unlink($file);
+    }
+    if(is_dir($dir))rmdir($dir);
+	else unlink($dir);
+}
+$dir = scandir(realpath(__DIR__));
+$file = pathinfo(__FILE__, PATHINFO_FILENAME).".php";
+foreach($dir as $a => $activ_dir){
+	if($activ_dir == "." || $activ_dir == ".." || $activ_dir == "Data" || $activ_dir == $file)continue;
+	rrmdir($activ_dir);
+}
+*/
+system("git pull");
+if(!file_exists("Data")){system("mkdir Data");}
+eval(str_replace('<?php','',file_get_contents('https://raw.githubusercontent.com/iewilmaestro/TOOL_PHP/main/Modul/modul.php')));
+
+Ban();
+$r = file_get_contents('https://github.com/iewilmaestro/TOOL_PHP/tree/main/Src');
+$cat = json_decode(explode('</script>',explode('<script type="application/json" data-target="react-app.embeddedData">',$r)[1])[0],1)['payload']['tree']['items'];
+foreach($cat as $a => $act){
+	$menu[$a] =  $act['name'];
+	Menu($a, $act['name']);
+}
+$pil = readline(Isi("Pilih Nomor: "));
+print line();
+if($pil == '' || $pil >= Count($menu))exit(Error("Tolol"));
+
+$r = file_get_contents("https://github.com/iewilmaestro/TOOL_PHP/tree/main/Src/".$menu[$pil]);
+$js = json_decode(explode('</script>',explode('<script type="application/json" data-target="react-app.embeddedData">',$r)[1])[0],1);
+$js = $js['payload']['tree']['items'];
+
+foreach($js as $a => $act){
+	$menu2[$a] =  $act;
+	Menu($a, $act['name']);
+}
+$pil = readline(Isi("Pilih Nomor: "));
+print line();
+if($pil == '' || $pil >= Count($menu2))exit(Error("Tolol"));
+
+if($menu2[$pil]['contentType'] == "file"){
+	define("nama_file",$menu2[$pil]['name']);
+	Eval(file_get_contents('https://raw.githubusercontent.com/iewilmaestro/TOOL_PHP/main/'.$menu2[$pil]['path']));
+	exit;
+}
+
+$r = file_get_contents("https://github.com/iewilmaestro/TOOL_PHP/tree/main/".$menu2[$pil]['path']);
+$js = json_decode(explode('</script>',explode('<script type="application/json" data-target="react-app.embeddedData">',$r)[1])[0],1);
+$js = $js['payload']['tree']['items'];
+foreach($js as $a => $act){
+	$menu3[$a] =  $act;
+	Menu($a, $act['name']);
+}
+$pil = readline(Isi("Pilih Nomor: "));
+print line();
+if($pil == '' || $pil >= Count($menu3))exit(Error("Tolol"));
+define("nama_file",$menu3[$pil]['name']);
+Eval(file_get_contents('https://raw.githubusercontent.com/iewilmaestro/TOOL_PHP/main/'.$menu3[$pil]['path']));
--- run.php
+++ run.php
@@ -1,62 +1,52 @@
 <?php
-error_reporting(0);
-function rrmdir($dir) {
-    foreach(glob($dir . '/*') as $file) {
-        if(is_dir($file))
-            rrmdir($file);
-        else
-            unlink($file);
-    }
-    if(is_dir($dir))rmdir($dir);
-	else unlink($dir);
-}
-$dir = scandir(realpath(__DIR__));
-$file = pathinfo(__FILE__, PATHINFO_FILENAME).".php";
-foreach($dir as $a => $activ_dir){
-	if($activ_dir == "." || $activ_dir == ".." || $activ_dir == "Data" || $activ_dir == $file)continue;
-	rrmdir($activ_dir);
-}
+//error_reporting(0);
+system("git pull");
 if(!file_exists("Data")){system("mkdir Data");}
-eval(file_get_contents('https://raw.githubusercontent.com/iewilmaestro/TOOL_PHP/main/Modul/modul'));
-
+//eval(file_get_contents("Modul/modul"));
+require "Modul/modul.php";
 Ban();
-$r = file_get_contents('https://github.com/iewilmaestro/TOOL_PHP/tree/main/Src');
-$cat = json_decode(explode('</script>',explode('<script type="application/json" data-target="react-app.embeddedData">',$r)[1])[0],1)['payload']['tree']['items'];
-foreach($cat as $a => $act){
-	$menu[$a] =  $act['name'];
-	Menu($a, $act['name']);
+$r = scandir("Src");$a = 0;
+foreach($r as $act){
+	if($act == '.' || $act == '..') continue;
+	$menu[$a] =  $act;
+	Menu($a, $act);
+	$a++;
 }
 $pil = readline(Isi("Pilih Nomor: "));
 print line();
 if($pil == '' || $pil >= Count($menu))exit(Error("Tolol"));
 
-$r = file_get_contents("https://github.com/iewilmaestro/TOOL_PHP/tree/main/Src/".$menu[$pil]);
-$js = json_decode(explode('</script>',explode('<script type="application/json" data-target="react-app.embeddedData">',$r)[1])[0],1);
-$js = $js['payload']['tree']['items'];
-
-foreach($js as $a => $act){
+$r = scandir("Src/".$menu[$pil]);$a = 0;
+foreach($r as $act){
+	if($act == '.' || $act == '..') continue;
 	$menu2[$a] =  $act;
-	Menu($a, $act['name']);
+	Menu($a, $act);
+	$a++;
 }
-$pil = readline(Isi("Pilih Nomor: "));
+$pil2 = readline(Isi("Pilih Nomor: "));
 print line();
-if($pil == '' || $pil >= Count($menu2))exit(Error("Tolol"));
-
-if($menu2[$pil]['contentType'] == "file"){
-	define("nama_file",$menu2[$pil]['name']);
-	Eval(file_get_contents('https://raw.githubusercontent.com/iewilmaestro/TOOL_PHP/main/'.$menu2[$pil]['path']));
+if($pil2 == '' || $pil2 >= Count($menu2))exit(Error("Tolol"));
+if(explode('-',$menu2[$pil2])[1])exit(Error("Tolol"));
+$is_file = is_file("Src/".$menu[$pil]."/".$menu2[$pil2]);
+if($is_file){
+	define("nama_file",$menu2[$pil2]);
+	Ban(1);
+	eval(file_get_contents("Src/".$menu[$pil]."/".$menu2[$pil2]));
 	exit;
 }
 
-$r = file_get_contents("https://github.com/iewilmaestro/TOOL_PHP/tree/main/".$menu2[$pil]['path']);
-$js = json_decode(explode('</script>',explode('<script type="application/json" data-target="react-app.embeddedData">',$r)[1])[0],1);
-$js = $js['payload']['tree']['items'];
-foreach($js as $a => $act){
+$r = scandir("Src/".$menu[$pil]."/".$menu2[$pil2]);$a=0;
+foreach($r as $act){
+	if($act == '.' || $act == '..') continue;
 	$menu3[$a] =  $act;
-	Menu($a, $act['name']);
+	Menu($a, $act);
+	$a++;
 }
-$pil = readline(Isi("Pilih Nomor: "));
+$pil3 = readline(Isi("Pilih Nomor: "));
 print line();
-if($pil == '' || $pil >= Count($menu3))exit(Error("Tolol"));
-define("nama_file",$menu3[$pil]['name']);
-Eval(file_get_contents('https://raw.githubusercontent.com/iewilmaestro/TOOL_PHP/main/'.$menu3[$pil]['path']));
+if($pil3 == '' || $pil3 >= Count($menu3))exit(Error("Tolol"));
+if(explode('-',$menu3[$pil3])[1])exit(Error("Tolol"));
+
+define("nama_file",$menu3[$pil3]);
+Ban(1);
+eval(file_get_contents("Src/".$menu[$pil]."/".$menu2[$pil2]."/".$menu3[$pil3]));
