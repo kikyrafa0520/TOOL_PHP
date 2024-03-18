03528dfea69ac9e674b15503d6cfa36da4621f2c
iewilmaestro <purna.iera@gmail.com>
2024-03-18 18:56:45 +0700

    Bagico,KeranCo

--- Modul/modul.php
+++ Modul/modul.php
@@ -241,6 +241,13 @@ function His($newdata,$data=0){
 	}
 	return array_merge($data,$newdata);
 }
+function cfDecodeEmail($encodedString){
+  $k = hexdec(substr($encodedString,0,2));
+  for($i=2,$email='';$i<strlen($encodedString)-1;$i+=2){
+    $email.=chr(hexdec(substr($encodedString,$i,2))^$k);
+  }
+  return $email;
+}
 /*************************** APIKEY ***************************/
 function Simpan_Api($nama_data){
 	if(file_exists("Data/Apikey/".$nama_data)){
