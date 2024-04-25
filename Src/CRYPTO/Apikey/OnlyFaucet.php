940d77a00312cb54c17ea3170502113bd554a43f
iewilmaestro <purna.iera@gmail.com>
2024-04-25 17:25:47 +0700

    earnfreebtc,altcrypt,earncryptowrs

--- Src/CRYPTO/Apikey/OnlyFaucet.php
+++ Src/CRYPTO/Apikey/OnlyFaucet.php
@@ -13,6 +13,33 @@ function h($data=0){
 	$h[] = "Cookie: ".simpan("Cookie");
 	return $h;
 }
+function Firewall(){
+	global $api;
+	while(1){
+		$r = curl(host."firewall",h())[1];
+		$csrf = explode('"',explode('name="csrf_token_name" value="',$r)[1])[0];
+		$captcha = explode('"',explode('name="captchaType" value="',$r)[1])[0];
+		$turnstile = explode('"',explode('<div class="cf-turnstile" data-sitekey="',$r)[1])[0];
+		$recap = explode('"',explode('<div class="g-recaptcha" data-sitekey="',$r)[1])[0];
+		if($turnstile){
+			$cap = $api->Turnstile($turnstile, host."firewall");
+			$data["cf-turnstile-response"] = $cap;
+		}else
+		if($recap){
+			$cap = $api->RecaptchaV2($recap, host."firewall");
+			$data["g-recaptcha-response"] = $cap;
+		}else{
+			continue;
+		}
+		if(!$cap)continue;
+		$data["captchaType"] = $captcha;
+		$data["csrf_token_name"] = $csrf;
+		$r = curl(host."firewall/verify",h(),http_build_query($data))[1];
+		if(preg_match('/Invalid Captcha/',$r))continue;
+		Cetak("Firewall","Bypassed");
+		return 0;
+	}
+}
 function login($email){
 	ulang:
 	if($email == "purna.iera@gmail.com"){
@@ -83,8 +110,9 @@ while(true){
 		if($a == 0)continue;
 		$coin = explode('"',$coins)[0];
 		$r = curl(host."faucet/currency/".$coin,h())[1];
+		if(preg_match('/Firewall/',$r)){firewall();continue;}
 		if(preg_match('/An uncaught Exception was encountered/',$r)){print Error("An uncaught Exception was encountered\n");sleep(2);print "\r                                 \r";tmr(60);continue;}
-		if(preg_match('/Just moment/',$r)){exit(Error("Cloudflare\n"));}
+		if(preg_match('/Just moment/',$r)){hapus("Cookie");print Error("Cloudflare\n");goto cookie;}
 		if(preg_match('/Please confirm your email address to be able to claim or withdraw/',$r)){print Error("Please confirm your email address to be able to claim or withdraw\n");print line();exit;}
 		if($res){
 			if($res[$coin] > 2)continue;
@@ -103,14 +131,17 @@ while(true){
 		$tmr = explode("-",explode('var wait = ',$r)[1])[0];
 		$sitekey = explode('"',explode('<div class="cf-turnstile" data-sitekey="',$r)[1])[0];
 		$sisa = explode('</span>',explode('<span class="badge badge-info">',$r)[1])[0];
-		if(!$sitekey){print Error("Sitekey Error\n");continue;}
+		if($sitekey){
+			$cap = $api->Turnstile($sitekey, host."faucet/currency/".$coin);
+			if(!$cap)continue;
+			$data = "csrf_token_name=".$csrf."&token=".$hiden."&captcha=turnstile&cf-turnstile-response=".$cap;
+		}else{
+			$data = "csrf_token_name=".$csrf."&token=".$hiden;
+		}
 		if($tmr){
 			tmr($tmr);
 		}
-		$cap = $api->Turnstile($sitekey, host."faucet/currency/".$coin);
-		if(!$cap)continue;
 		
-		$data = "csrf_token_name=".$csrf."&token=".$hiden."&captcha=turnstile&cf-turnstile-response=".$cap;
 		$r = curl(host."faucet/verify/".$coin,h(),$data)[1];
 		$ban = explode('</div>',explode('<div class="alert text-center alert-danger"><i class="fas fa-exclamation-circle"></i> Your account',$r)[1])[0];
 		$ss = explode("account!",explode("html: '0.",$r)[1])[0];
@@ -137,16 +168,13 @@ while(true){
 			$res = his([$coin=>1],$res);
 		}else{
 			print Error("Server Down\n");
-			hapus("cookie.txt");
-			goto login;
 			sleep(3);
 			print "\r                  \r";
 			$res = his([$coin=>1],$res);
 		}
 	}
 	if(!$res){
-		hapus("Cookie");
-		goto cookie; 
+		continue;
 	}
 	if(min($res) > 2)break;
 }
